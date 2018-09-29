558280a513bf7bb70f17029ffdd9dafb324e73ea
James Ezell <Ezell_JamesP40@outlook.com>
2018-09-29 14:40:30 -0700

    9/29 Homework

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,10 +11,20 @@ namespace PartyInvites.Controllers
     public class HomeController : Controller
     {
        public ViewResult Index()
-        {
+       {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
+       }
+
+        [HttpGet]
+        public ViewResult RsvpForm() => View();
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks" , guestResponse);
         }
     }
 }
